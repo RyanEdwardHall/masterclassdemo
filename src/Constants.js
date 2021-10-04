81ccb8ad6ff3820950b779fb0708fbf0d6010d8c
Ryan Hall <rhall@binsentry.com>
2021-10-04 14:47:44 -0400

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+const Constants = {
+  API: {
+    API_BASE: "https://mc-dev-5.herokuapp.com/",
+    GET_ALL_COURSES: "https://mc-dev-5.herokuapp.com/jsonapi/v1​/courses",
+  },
+};
+
+export default Constants;
